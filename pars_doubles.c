d76f68f952dd8844396128c7c49aa4053abad98d
Igors78 <oleinikovsigors@gmail.com>
2021-11-17 16:44:45 +0200

    Fix open map

--- pars_doubles.c
+++ pars_doubles.c
@@ -6,11 +6,33 @@
 /*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:42:08 by ioleinik          #+#    #+#             */
-/*   Updated: 2021/11/17 15:16:20 by ioleinik         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:41:33 by ioleinik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+static int	open_map(char **a, int i, int k)
+{
+	if (a[i][k] == '0' && (a[i][k - 1] == ' ' || a[i][k + 1] == ' '))
+		return (1);
+	if (a[i][k] == '0' && (k > (int)ft_strlen(a[i - 1])
+	|| k > (int)ft_strlen(a[i + 1])))
+		return (1);
+	if (k + 1 < (int)ft_strlen(a[i - 1]))
+	{
+		if (a[i][k] == '0' && (a[i - 1][k - 1] == ' '
+			|| a[i - 1][k] == ' ' || a[i - 1][k + 1] == ' '))
+			return (1);
+	}
+	if (k + 1 < (int)ft_strlen(a[i + 1]))
+	{
+		if (a[i][k] == '0' && (a[i + 1][k - 1] == ' ' || a[i + 1][k] == ' '
+			|| a[i + 1][k + 1] == ' '))
+			return (1);
+	}
+	return (0);
+}
 
 static void	dubl_symb(char **a, t_cub *d)
 {
@@ -50,11 +72,7 @@ void	parse_doubles(char **a, t_cub *d)
 		k = 1;
 		while (k < ft_strlen(a[i]) - 1)
 		{
-			if (a[i][k] == '0' && (a[i - 1][k - 1] == ' '
-			|| a[i - 1][k] == ' ' || a[i - 1][k + 1] == ' '
-			|| a[i][k - 1] == ' ' || a[i][k + 1] == ' '
-			|| a[i + 1][k - 1] == ' ' || a[i + 1][k] == ' '
-			|| a[i + 1][k + 1] == ' '))
+			if (open_map(a, i, k))
 				inv_map(d);
 			k++;
 		}
